6df8377d169ba628a27646267064ae89364387d9
“JiangYu” <lnsyyj@hotmail.com>
2018-01-15 23:07:46 +0800

    update

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ easy_install pip
 pip install --upgrade pip
 pip install --upgrade setuptools
 sudo pip install virtualenv
+virtualenv ./virtualenv
+./virtualenv/bin/pip install --upgrade pip
+./virtualenv/bin/pip install setuptools --upgrade
