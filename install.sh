ccf5e478abff646c02baae3a9a9a5470cdc685fd
“JiangYu” <lnsyyj@hotmail.com>
2018-01-15 23:18:24 +0800

    update

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ sudo pip install virtualenv
 virtualenv ./virtualenv
 ./virtualenv/bin/pip install --upgrade pip
 ./virtualenv/bin/pip install setuptools --upgrade
+./virtualenv/bin/pip install request==0.0.22
+./virtualenv/bin/pip install awsauth
