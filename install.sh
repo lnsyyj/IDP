dbe8bb1aeb93488c1ab88b08e48c2918b65de5b0
“JiangYu” <lnsyyj@hotmail.com>
2018-01-21 21:11:47 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 easy_install pip
 pip install --upgrade pip
 pip install --upgrade setuptools
-sudo pip install virtualenv
-virtualenv ./virtualenv
-./virtualenv/bin/pip install --upgrade pip
-./virtualenv/bin/pip install setuptools --upgrade
-./virtualenv/bin/pip install boto==2.48.0
-./virtualenv/bin/pip install logger==1.4
+#sudo pip install virtualenv
+#virtualenv ./virtualenv
+pip install --upgrade pip
+pip install setuptools --upgrade
+pip install boto==2.48.0
+pip install logger==1.4
 
-./virtualenv/bin/pip install request==0.0.22
-./virtualenv/bin/pip install awsauth==0.3.3
+pip install request==0.0.22
+pip install awsauth==0.3.3
+yum install mysql-devel gcc python-devel -y
+pip install MySQL-python
